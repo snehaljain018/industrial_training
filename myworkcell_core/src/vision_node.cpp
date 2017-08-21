ff137bae3748893040150cc8177f96762035ff25
Ryan <snehaljain018@gmail.com>
2017-08-21 14:40:30 +0800

    Added subscriber node

--- myworkcell_core/src/vision_node.cpp
+++ myworkcell_core/src/vision_node.cpp
@@ -1,6 +1,7 @@
 /** Simple ROS Node **/
 
 #include<ros/ros.h>
+#include<fake_ar_publisher/ARMarker.h>
  
 int main(int argc, char* argv[])
 {
@@ -8,7 +9,31 @@ int main(int argc, char* argv[])
 
 	ros::NodeHandle nh;
 
-	ROS_INFO_STREAM("Hello World!");
+	class Localizer
+	{
+	public:
+		Localizer(ros::NodeHandle& nh)
+		{
+			ar_sub_ = nh.subscribe<fake_ar_publisher::ARMarker>("ar_pose_marker", 1, &Localizer::visionCallback, this);
+  		}
+
+		void visionCallback(const fake_ar_publisher::ARMarkerConstPtr& msg)
+		{
+			last_msg_ = msg;
+			ROS_INFO_STREAM(last_msg_->pose.pose);
+		}
+
+		ros::Subscriber ar_sub_;
+		fake_ar_publisher::ARMarkerConstPtr last_msg_;
+	};
+
+
+	Localizer localizer(nh);
+
+	ROS_INFO_STREAM("Starting vision node!");
 
 	ros::spin();
 }
+
+
+
