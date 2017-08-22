444e12907b40725d6f6e988833e03b87b1d7e24f
Ryan <snehaljain018@gmail.com>
2017-08-22 23:21:16 +0800

    Reverting to working version

--- myworkcell_core/src/myworkcell_node.cpp
+++ myworkcell_core/src/myworkcell_node.cpp
@@ -2,14 +2,18 @@
 #include "myworkcell_core/LocalizePart.h"
 #include "tf/tf.h"
 #include <moveit/move_group_interface/move_group_interface.h>
+#include <actionlib/client/simple_action_client.h>
+#include <control_msgs/FollowJointTrajectoryAction.h>
+#include <myworkcell_core/PlanCartesianPath.h>
 
 
 class ScanNPlan
 {
 public:
-  ScanNPlan(ros::NodeHandle& nh)
+  ScanNPlan(ros::NodeHandle& nh) : ac_("joint_trajectory_action", true)
   {
     vision_client_ = nh.serviceClient<myworkcell_core::LocalizePart>("localize_part");
+    cartesian_client_ = nh.serviceClient<myworkcell_core::PlanCartesianPath>("plan_path");
   }
 
   void start(const std::string& base_frame)
@@ -31,11 +35,30 @@ public:
     geometry_msgs::Pose move_target = srv.response.pose;
     move_group.setPoseTarget(move_target);
     move_group.move();
+
+    // Plan cartesian path
+    myworkcell_core::PlanCartesianPath cartesian_srv;
+    cartesian_srv.request.pose = move_target;
+    if (!cartesian_client_.call(cartesian_srv))
+    {
+      ROS_ERROR("Could not plan for path");
+      return;
+    }
+
+    // Execute descartes-planned path directly (bypassing MoveIt)
+    ROS_INFO("Got cart path, executing");
+    control_msgs::FollowJointTrajectoryGoal goal;
+    goal.trajectory = cartesian_srv.response.trajectory;
+    ac_.sendGoal(goal);
+    ac_.waitForResult();
+    ROS_INFO("Done");
   }
 
 private:
   // Planning components
   ros::ServiceClient vision_client_;
+  ros::ServiceClient cartesian_client_;
+  actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> ac_;
 };
 
 int main(int argc, char **argv)
