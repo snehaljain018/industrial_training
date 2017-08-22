7f83aabe80bae7cbedb3676b055a83f931da0217
Ryan <snehaljain018@gmail.com>
2017-08-22 11:27:18 +0800

    Adding URDF and xacro definitions

--- myworkcell_core/src/myworkcell_node.cpp
+++ myworkcell_core/src/myworkcell_node.cpp
@@ -1,9 +1,6 @@
 #include "ros/ros.h"
 #include "myworkcell_core/LocalizePart.h"
 
-#include <ros/ros.h>
-#include <myworkcell_core/LocalizePart.h>
-
 class ScanNPlan
 {
 public:
@@ -12,11 +9,14 @@ public:
     vision_client_ = nh.serviceClient<myworkcell_core::LocalizePart>("localize_part");
   }
 
-  void start()
+  void start(const std::string& base_frame)
   {
     ROS_INFO("Attempting to localize part");
     // Localize the part
     myworkcell_core::LocalizePart srv;
+    srv.request.base_frame = base_frame;
+    ROS_INFO_STREAM("Requesting pose in base frame: " << base_frame);
+
     if (!vision_client_.call(srv))
     {
       ROS_ERROR("Could not localize part");
@@ -34,12 +34,16 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "myworkcell_node");
   ros::NodeHandle nh;
+  ros::NodeHandle private_node_handle ("~");
+  std::string base_frame;
+
+  private_node_handle.param<std::string>("base_frame", base_frame, "world");
 
   ROS_INFO("ScanNPlan node has been initialized");
 
   ScanNPlan app(nh);
   ros::Duration(.5).sleep();  // wait for the class to initialize
-  app.start();
+  app.start(base_frame);
 
   ros::spin();
 }
